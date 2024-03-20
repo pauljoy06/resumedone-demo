c71858c34568f5deb4aba50d5b963c6295d7223c
Paul Padamadan <paul@zephony.com>
2024-03-20 12:17:00 +0530

    Initial folders and setup

---
+++ src/components/index.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+
+
+function Icon(props) {
+    let width, height;
+
+    if (props.size) {
+        width = height = props.size;
+    } else if (props.width || props.height) {
+        if (props.width) {
+            width = props.width;
+        }
+
+        if (props.height) {
+            height = props.height;
+        }
+    } else {
+        width = height = 12;
+    }
+
+    return <div className='icon'>
+        <div onClick={props.onClick}
+            className={`icon-image ${props.className || ''}`}
+            style={{
+                width: width ? width + 'px' : undefined,
+                height: height ? height + 'px' : undefined,
+                backgroundImage: `url(${props.image})`,
+                backgroundRepeat: 'no-repeat',
+                backgroundPosition: 'center',
+                backgroundSize: `${width}px ${height}px`,
+                ...props.style,
+            }}
+        >
+        </div>
+    </div>
+}
+
+export {
+    Icon,
+}
